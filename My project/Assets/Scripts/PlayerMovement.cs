b7c0084b22a1e4e56179da8feb4ded0f9e87a5b7
casalessandro <ale.casale3@stud.uniroma3.it>
2023-10-21 17:55:18 +0200

    Aggiunte animazioni e script per la fotocamera

---
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    private float dirX= 0f;
+    [SerializeField] private float moveSpeed = 7f; //Velocità movimenti orizzontali;
+    [SerializeField] private float jumpForce = 14f; //Velocità salto; Serialize indica che la propirietà appare anche su unity(puoi mettere pure public);
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim= GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        UpdateAnimationUpdate();
+    }
+    private void UpdateAnimationUpdate() //Si mette update per far capi che si deve eseguire ogni frame
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("Running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("Running", true);
+            sprite.flipX = true;
+        }
+        else
+            anim.SetBool("Running", false);
+    }
+}
